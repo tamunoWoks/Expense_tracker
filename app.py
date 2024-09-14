d762726f3e0449f4b5eb7add4ae71d775f6746e2
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-09-14 08:43:44 -0600

    Import modules

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
 # Install Flask
 pip install flask
+
+# Import modules
+from flask import Flask, jsonify, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expenses.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
