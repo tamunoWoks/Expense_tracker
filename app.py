cc8aa6d1a70874e7370176e6401f3a865e2c1ff0
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2024-09-14 08:46:59 -0600

    Expense model for the database

--- app.py
+++ app.py
@@ -10,3 +10,13 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expenses.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+# Expense model for the database
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(200), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Expense {self.id}>'
